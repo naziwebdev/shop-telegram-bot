5282c0dc2d826e6fba3980ae10817ebabc846ec9
nazi <nazipaeez777@gmail.com>
2024-10-27 00:31:32 +0330

    Do logic of send hint message

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 const { Telegraf, Markup } = require("telegraf");
 const configs = require("./configs");
 const { mainMenu, buyMenu, choiceCountMenu } = require("./utils/Menues");
-
+const { hintMessage } = require("./messages");
 const token = configs.telegramToken;
 
 const bot = new Telegraf(token);
 
+//var for choice count of product
 let discountCount = 1;
 let name;
 let price;
 
+//start
 bot.start((ctx) => {
   mainMenu(ctx);
 });
 
+//send products list keyborad menu
 bot.hears("خرید محصول 🛍️", (ctx) => {
   buyMenu(ctx);
 });
 
+//send hint message
+bot.hears("راهنما ✅", (ctx) => {
+  ctx.reply(hintMessage);
+});
+
 //this must be above the callback_query otherwise dont work
+//send buy keyboard menu
 bot.action("menu", (ctx) => {
   buyMenu(ctx);
 });
 
+// plus count of product when buy
 bot.action("plus", (ctx) => {
   discountCount = discountCount + 1;
   choiceCountMenu(ctx, name, price, discountCount);
 });
 
+// minus count of product when buy
 bot.action("minus", (ctx) => {
   discountCount = discountCount - 1;
   choiceCountMenu(ctx, name, price, discountCount);
 });
 
+//send choice count of product keyboard menu
 bot.on("callback_query", (ctx) => {
   const cammand = ctx.callbackQuery.data;
   const actions = ["meat", "fruit", "food", "sweet"];
