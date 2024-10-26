55ce24e06d9124bccab98d643f088554dd40ebc5
nazi <nazipaeez777@gmail.com>
2024-10-26 23:58:40 +0330

    Create buy menu & choicrCount menu

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const { Telegraf} = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const configs = require("./configs");
-const { mainMenue } = require("./utils/Menues");
+const { mainMenu, buyMenu, choiceCountMenu } = require("./utils/Menues");
 
 const token = configs.telegramToken;
 
 const bot = new Telegraf(token);
 
 bot.start((ctx) => {
-  mainMenue(ctx);
+  mainMenu(ctx);
+});
+
+bot.hears("خرید محصول 🛍️", (ctx) => {
+  buyMenu(ctx);
+});
+
+bot.on("callback_query", (ctx) => {
+  const cammand = ctx.callbackQuery.data;
+  const actions = ["meat", "fruit", "food", "sweet"];
+
+  let name;
+  let price;
+
+  switch (cammand) {
+    case "meat":
+      name = "پروتیین";
+      price = 4000;
+      break;
+    case "fruit":
+      name = "میوه";
+      price = 3000;
+      break;
+    case "food":
+      name = "غذا";
+      price = 6000;
+      break;
+    case "sweet":
+      name = "شیرینی";
+      price = 5000;
+      break;
+    default:
+      name = "محصول";
+      price = 0;
+  }
+
+  if (actions.includes(cammand)) {
+    choiceCountMenu(ctx, name, price);
+  }
 });
 
 bot.launch();
--- utils/Menues.js
+++ utils/Menues.js
@@ -1,17 +1,42 @@
 const { Markup } = require("telegraf");
 
-const mainMenue = (ctx) => {
+const mainMenu = (ctx) => {
   ctx.reply(
     "سلام به ربات دریافت کد تخفیف خوش آمدید  ❤️ \n در صورت هر گونه سو استفاده از ربات  دسترسی شما مسدود خواهد شد ✖️",
     Markup.keyboard([
-      [Markup.button.callback("خرید محصول 🛍️", "buy")],
-      [
-        Markup.button.callback("پشتیبانی 💁‍♀️", " support"),
-        Markup.button.callback("حساب کاربری 👤", "account"),
-      ],
-      [Markup.button.callback("راهنما ✅", "guide")],
+      ["خرید محصول 🛍️"],
+      ["پشتیبانی 💁‍♀️", "حساب کاربری 👤"],
+      ["راهنما ✅"],
     ])
   );
 };
 
-module.exports = { mainMenue };
+const buyMenu = (ctx) => {
+  ctx.reply(
+    "محصول مورد نظر را انتخاب کنید :",
+    Markup.inlineKeyboard([
+      [Markup.button.callback(" کد پروتئین 🍗", "meat")],
+      [Markup.button.callback("کد میوه 🍏", "fruit")],
+      [Markup.button.callback("کد غذا 🍕", "food")],
+      [Markup.button.callback("کد شیرینی 🧁", "sweet")],
+    ])
+  );
+};
+
+const choiceCountMenu = (ctx, name, price) => {
+  ctx.editMessageText(
+    `خرید کد${name} به مبلغ : ${price} تومان\n تعداد را مشخص کنید \n تعداد:1 `,
+    Markup.inlineKeyboard([
+      [
+        Markup.button.callback("+1", "plus"),
+        Markup.button.callback("-1", "minus"),
+      ],
+      [
+        Markup.button.callback("تایید خرید ✅", "accept"),
+        Markup.button.callback("منو اصلی 🏠", "menu"),
+      ],
+    ])
+  );
+};
+
+module.exports = { mainMenu, buyMenu, choiceCountMenu };
